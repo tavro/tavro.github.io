1b4701e61aa1b12829ef42e7f49bdc27a1a4490f
Isak Horvath <isakhorvath@gmail.com>
2022-02-14 12:03:05 +0100

    NEW: Add song texts

--- src/pages/Project/Songs/InLoveIndeed.js
+++ src/pages/Project/Songs/InLoveIndeed.js
@@ -9,15 +9,35 @@ class InLoveIndeed extends React.Component {
 	      <Header index={1} content={this.props.content} logged_in={this.props.logged_in}/>
 	      <main>
 	        <h2>In Love Indeed</h2>
-	        <p>blah blah blah</p>
+	        <p>Description will come soon...</p>
 	        <div class="paper">
 	            <h3>In Love Indeed</h3>
 	            <p>[Intro]</p>
+	            <p>Let's consider me and you</p>
+	            <p>Reconsider what to do</p>
 	            <p>[Chorus]</p>
+	            <p>Sadness comes then suddenly goes</p>
+	            <p>When the music starts to beat</p>
+	            <p>Happiness comes and suddenly grows</p>
+	            <p>My favorite love songs on repeat</p>
 	            <p>[Hook]</p>
+	            <p>One kiss is all I need</p>
+	            <p>I'm in love indeed</p>
+	            <p>One hug is all I need</p>
+	            <p>I'm in love indeed</p>
 	            <p>[Chorus]</p>
+	            <p>Sadness comes then suddenly goes</p>
+	            <p>When the music starts to beat</p>
+	            <p>Happiness comes and suddenly grows</p>
+	            <p>My favorite love songs on repeat</p>
 	            <p>[Bridge]</p>
+	            <p>(Just be with me)</p>
+	            <p>(Just breathe with me)</p>
 	            <p>[Hook]</p>
+	            <p>One kiss is all I need</p>
+	            <p>I'm in love indeed</p>
+	            <p>One hug is all I need</p>
+	            <p>I'm in love indeed</p>
 	        </div>
 	      </main>
 	</div>
--- src/pages/Project/Songs/SafeWithMe.js
+++ src/pages/Project/Songs/SafeWithMe.js
@@ -13,9 +13,25 @@ class SafeWithMe extends React.Component {
 	        <div class="paper">
 	            <h3>Safe With Me</h3>
 	            <p>[Chorus]</p>
+	            <p>Don't go babe</p>
+	            <p>Where I cant be babe</p>
+	            <p>Your heart babe</p>
+	            <p>It's safe with me babe</p>
 	            <p>[Verse 1]</p>
+	            <p>Hold tight babe</p>
+	            <p>Feels right babe</p>
+	            <p>Dim light babe</p>
+	            <p>Let's unite babe</p>
 	            <p>[Chorus]</p>
+	            <p>Don't go babe</p>
+	            <p>Where I cant be babe</p>
+	            <p>Your heart babe</p>
+	            <p>It's safe with me babe</p>
 	            <p>[Verse 2]</p>
+	            <p>No clothes babe</p>
+	            <p>Come close babe</p>
+	            <p>Time slows babe</p>
+	            <p>Let's expose babe</p>
 	        </div>
 	      </main>
 	</div>
